58232f208238e020c0cf7c7bc0441bfac48bd120
solangeguzman <solangeguzmanduchen@gmail.com>
2021-06-01 16:52:19 +0200

    codice

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,12 @@
 for (i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 !== 0) 
+    if (i % 3 === 0 && i % 5 !== 0) {
         document.getElementById("numero").innerHTML += "..." + "Fizz";
-    } else if (i % 5 === 0  && i % 3 === 0) {
-        document.getElementById("numero").innerHTML += "..." + "buzz";  
-    } else if (i % 3 === 0 && i % 5 === 0){
+    } else if (i % 3 === 0){
+        document.getElementById("numero").innerHTML += "..." + "Buzz";  
+    } else if (i % 5 === 0){
         document.getElementById("numero").innerHTML += "..." + "FizzBuzz";
     } else{
         document.getElementById("numero").innerHTML += "..." + i; 
     }
 }
+
