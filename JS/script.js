42d687e2574a612b82a9e16c722a6e235afba26f
solangeguzman <solangeguzmanduchen@gmail.com>
2021-06-01 15:47:20 +0200

    codice

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 for (i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 !== 0) 
         document.getElementById("numero").innerHTML += "..." + "Fizz";
-    } else if (i % 5 === 0  && x % 3 === 0) {
+    } else if (i % 5 === 0  && i % 3 === 0) {
         document.getElementById("numero").innerHTML += "..." + "buzz";  
     } else if (i % 3 === 0 && i % 5 === 0){
         document.getElementById("numero").innerHTML += "..." + "FizzBuzz";
